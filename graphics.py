6a62d9470c1a51359decd56c6490625f0f5f7432
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2024-01-28 19:17:35 +1100

    Drawing a line

--- graphics.py
+++ graphics.py
@@ -1,17 +1,40 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+
+class Line:
+
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas: Canvas, fill_color: str = "black"):
+        canvas.create_line(
+            self.p1.x,
+            self.p1.y,
+            self.p2.x,
+            self.p2.y,
+            fill=fill_color,
+            width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
+
+
 class Window:
 
     def __init__(self, width: float, height: float):
-        self.width = width
-        self.height = height
         self.__root = Tk()
         self.__root.title("Mazer")
         self.__canvas = Canvas(self.__root,
                                background="cyan",
-                               width=self.width,
-                               height=self.height)
+                               width=width,
+                               height=height)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__is_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -26,6 +49,9 @@ class Window:
         while self.__is_running:
             self.redraw()
         print("Closing window.")
+
+    def draw_line(self, line: Line, fill_color: str = "black"):
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__is_running = False
